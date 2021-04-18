b0be0b99a56d44889308e36c2ab21fddc4cd6199
Petro Prydorozhnyi <petro@stakelogic.com>
2021-04-18 14:41:58 +0300

    fixed

--- src/main/resources/db/changelog/changelog/sql/init_schema.sql
+++ src/main/resources/db/changelog/changelog/sql/init_schema.sql
@@ -1,8 +1,14 @@
+CREATE TABLE pr_name_holder
+(
+    id  SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
+
 CREATE TABLE pr_session
 (
     session_id SERIAL PRIMARY KEY,
     players JSONB,
-    estimate INTEGER,
+    estimated INTEGER,
     active_round_id INTEGER,
     result INTEGER
 );
