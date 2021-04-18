fca37912e79fb252893a1c273e1ce9acf0f6e601
Petro Prydorozhnyi <petro@stakelogic.com>
2021-04-18 12:39:13 +0300

    custom types

---
+++ src/main/java/com/example/messagingstompwebsocket/model/entities/EntityConst.java
@@ -0,0 +1,15 @@
+package com.example.messagingstompwebsocket.model.entities;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class EntityConst {
+
+  public static final String PG_ENUM_TYPE = "enum";
+  public static final String PG_JSONB_TYPE = "jsonb";
+  public static final String PG_INT_ARRAY_TYPE = "int_array";
+  public static final String PG_LONG_ARRAY_TYPE = "long_array";
+  public static final String PG_STRING_ARRAY_TYPE = "string_array";
+  public static final String PG_DOUBLE_ARRAY_TYPE = "double_array";
+
+}
---
+++ src/main/java/com/example/messagingstompwebsocket/model/entities/package-info.java
@@ -0,0 +1,24 @@
+@org.hibernate.annotations.TypeDef(name = EntityConst.PG_ENUM_TYPE, typeClass =
+    PostgreSQLEnumType.class)
+@org.hibernate.annotations.TypeDef(name = EntityConst.PG_JSONB_TYPE, typeClass =
+    JsonBinaryType.class)
+@org.hibernate.annotations.TypeDef(name = EntityConst.PG_INT_ARRAY_TYPE, typeClass =
+    IntArrayType.class)
+@org.hibernate.annotations.TypeDef(name = EntityConst.PG_LONG_ARRAY_TYPE, typeClass =
+    LongArrayType.class)
+@org.hibernate.annotations.TypeDef(name = EntityConst.PG_STRING_ARRAY_TYPE, typeClass =
+    StringArrayType.class)
+@org.hibernate.annotations.TypeDef(name = EntityConst.PG_DOUBLE_ARRAY_TYPE, typeClass =
+    DoubleArrayType.class)
+package com.example.messagingstompwebsocket.model.entities;
+
+import com.vladmihalcea.hibernate.type.array.DoubleArrayType;
+import com.vladmihalcea.hibernate.type.array.IntArrayType;
+import com.vladmihalcea.hibernate.type.array.LongArrayType;
+import com.vladmihalcea.hibernate.type.array.StringArrayType;
+import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+
+
+
+
