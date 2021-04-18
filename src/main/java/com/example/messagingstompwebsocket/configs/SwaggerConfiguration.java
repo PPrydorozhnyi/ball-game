e13020538f596652be072aadeb5d601c42088fc7
Petro Prydorozhnyi <petro@stakelogic.com>
2021-04-18 14:57:40 +0300

    path

--- src/main/java/com/example/messagingstompwebsocket/configs/SwaggerConfiguration.java
+++ src/main/java/com/example/messagingstompwebsocket/configs/SwaggerConfiguration.java
@@ -20,7 +20,7 @@ public class SwaggerConfiguration {
   @Bean
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2).select()
-        .apis(RequestHandlerSelectors.any())
+        .apis(RequestHandlerSelectors.basePackage("com.example.messagingstompwebsocket.controllers"))
         .paths(PathSelectors.any())
         .build()
         .useDefaultResponseMessages(false)
